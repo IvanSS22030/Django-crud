9dfa1f5b49bc55787fd45b81378d86175b9c34fe
Ivan joel Sanchez Santana <ivanj_ss@hotmail.com>
2023-10-07 13:29:34 -0400

    Signing feature fixed

--- tasks/views.py
+++ tasks/views.py
@@ -2,7 +2,7 @@ from django.shortcuts import render, redirect
 from django.contrib.auth.forms import UserCreationForm, AuthenticationForm
 from django.contrib.auth.models import User
 from  django.db import IntegrityError
-from django.contrib.auth import login, logout
+from django.contrib.auth import login, logout, authenticate
 
 
 
@@ -51,5 +51,15 @@ def signin(request):
         'form': AuthenticationForm
     })
         
-   
+    else:
+        user = authenticate(request, username=request.POST['username'], password =request.POST['password'])
+        if user is None:
+            return render(request, 'signin.html',{
+        'form': AuthenticationForm,
+        'error': 'Username or password is incorect'
+        })
+            
+        return render(request, 'signin.html',{
+        'form': AuthenticationForm
+        })
     
